6126106812b04e2b425b78da570f256364cfa6fe
Levy Moreira <levymoreira.ce@gmail.com>
2016-10-22 23:23:06 -0300

    Add answer api to backend

--- backend/server/routes.js
+++ backend/server/routes.js
@@ -5,6 +5,7 @@ const errors = require('rest-api-errors')
 module.exports = function(app) {
 
   app.use('/api/lessons', require('./api/lesson'))
+  app.use('/api/answers', require('./api/answer'))
   
   app.use(function(req, resp, next) {
     throw new errors.NotImplemented()
