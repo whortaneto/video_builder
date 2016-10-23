c52f5317bf21e8cd78ed357b134e6726a6274507
Carlos Mendes Aderaldo <cmendesce@gmail.com>
2016-10-22 21:26:33 -0300

    simple refactoring

--- backend/server/errorHanding.js
+++ backend/server/errorHanding.js
@@ -1,33 +1,26 @@
-var APIError = require('rest-api-errors').APIError;
+const APIError = require('rest-api-errors').APIError
 
-module.exports = function(app) {
+module.exports = (app) => {
 
-    var env = app.get('env');
+  const env = app.get('env');
+  app.use(function (err, req, resp, next) {
 
-    app.use(function (err, req, resp, next) {
+    if (resp.headersSent) {
+      return next(err);
+    }
+    if (err instanceof APIError || err.status) {
+      if (err.code || err.message) {
+        return resp.status(err.status).send({code: err.code, message: err.message});
+      } else {
+        return resp.status(err.status).end();
+      }
+    }
 
-        if (resp.headersSent) {
-            return next(err);
-        }
-
-        if (err instanceof APIError || err.status) {
-            if(err.code || err.message) {
-                return resp.status(err.status).send({
-                    code: err.code,
-                    message: err.message
-                });
-            } else {
-                return resp.status(err.status).end();
-            }
-        }
-
-        console.error(err);
-        if(err && err.stack) {
-            console.error(err.stack);
-        }
-
-        return resp.status(500).send({
-            code: 'wtf'
-        });
-    });
-};
+    console.error(err);
+    if (err && err.stack) {
+      console.error(err.stack);
+    }
+    
+    return resp.status(500).send({code: 'Internal Server Error'})
+  })
+}
