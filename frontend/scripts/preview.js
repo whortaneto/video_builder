3095b59ee84169296867146b850fd38baca5bdc9
Aurindo Jorge <aurindo_jorge@atlantico.com.br>
2016-10-23 18:17:45 -0300

    Adding preview by action button

---
+++ frontend/scripts/preview.js
@@ -0,0 +1,18 @@
+function PreviewService() {
+
+window.onload = () => {
+	let preview = getURLParameter('preview');
+	if(preview!=undefined) {
+		CourseBuilder.video('390', '640', preview);
+	}else{
+		console.log("error");
+	}
+}
+
+  const getURLParameter = (name) => {
+    return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null;
+  }
+
+}
+
+const previewService = new PreviewService();
