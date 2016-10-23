18120506d587abb4737976371c48b2cc55a9e476
Levy Moreira <levymoreira.ce@gmail.com>
2016-10-23 15:12:47 -0300

    Add search anwsers in backend

--- frontend/scripts/answer/answer.service.js
+++ frontend/scripts/answer/answer.service.js
@@ -1,3 +1,20 @@
-/**
- * Created by levy on 22/10/16.
- */
+function AnswerService() {
+  const API_URL = "http://4c-video-builder.tk:9000/api/answers";
+
+  this.getAll = () => {
+    return fetch(API_URL)
+      .then(function(response) {
+        return response.json();
+      });
+  }
+
+  this.get = (id) => {
+    return fetch(API_URL+'/'+id)
+      .then(function(response) {
+        return response.json();
+      });
+  }
+
+}
+
+const lessonService = new LessonService();
