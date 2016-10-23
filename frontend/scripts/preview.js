ed73aae3f77cab9a3e87d09d81f53ff8316edd29
Aurindo Jorge <aurindo_jorge@atlantico.com.br>
2016-10-23 18:42:03 -0300

    To Wagner

--- frontend/scripts/preview.js
+++ frontend/scripts/preview.js
@@ -3,16 +3,23 @@ function PreviewService() {
 window.onload = () => {
 	let preview = getURLParameter('preview');
 	if(preview!=undefined) {
+
+
+
 		CourseBuilder.video('390', '640', preview);
 	}else{
 		console.log("error");
 	}
 }
 
+
+}
+
   const getURLParameter = (name) => {
     return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null;
   }
 
-}
+
+document.getElementBy('previewId').value  = getURLParameter('preview');
 
 const previewService = new PreviewService();
